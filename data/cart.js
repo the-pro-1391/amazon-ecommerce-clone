45b55c81d5f857aa70d88f4e4e20351e7f0f4560
the-pro-1391 <bangarumadhu1209@gmail.com>
2026-08-06 17:02:54 +0530

    Make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -50,3 +50,17 @@ export function removeFromCart(productId) {
   cart = newCart;
   savaTostorage();
 }
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+    cart.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      } 
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+
+    savaTostorage();
+}
