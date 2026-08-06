cd972e48531a1453d332d75753ee45ea28294d93
the-pro-1391 <bangarumadhu1209@gmail.com>
2026-08-06 18:49:17 +0530

    MAke payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,11 +3,8 @@ import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import {renderPaymentSummary} from './paymentSummary.js';
 
-
-const today =  dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
 
 
 export function renderOrderSummary() {
@@ -126,6 +123,8 @@ export function renderOrderSummary() {
           `.js-cart-item-contanier-${productId}`
         );
         container.remove();
+
+        renderOrderSummary();
       });
     });
 
@@ -135,6 +134,7 @@ export function renderOrderSummary() {
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       });
     });
 }
