23c666c757b432636371a6d75328ce9f8af36be3
the-pro-1391 <bangarumadhu1209@gmail.com>
2026-08-07 20:09:30 +0530

    Learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -64,3 +64,16 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
 
     savaTostorage();
 }
+
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
